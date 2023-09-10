17e07aba86b491c298f06da6160f55fae20c6ab7
Kai Lukowiak <kailukowiak@gmail.com>
2023-09-09 22:01:45 -0600

    exploding gradients

--- Transformers/hephaestus_jax.py
+++ Transformers/hephaestus_jax.py
@@ -6,6 +6,7 @@ from itertools import chain
 
 import jax
 import jax.numpy as jnp
+import optax
 import pandas as pd
 from flax import linen as nn
 from jax import random
@@ -72,6 +73,7 @@ class TabularDS:
     seed: int = 42
     # device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # special_tokens: list =
+    cat_mask_token: str = "[MASK]"
     special_tokens: list = field(
         default_factory=lambda: ["[PAD]", "[NUMERIC_MASK]", "[MASK]"]
     )
@@ -113,6 +115,9 @@ class TabularDS:
             X, y, test_size=0.2
         )
         self.n_tokens = len(self.tokens)
+        self.numeric_col_tokens = jnp.array(
+            [self.token_dict[i] for i in self.numeric_columns]
+        )
 
         X_train_numeric = self.X_train[self.numeric_columns]
         X_train_categorical = self.X_train[self.category_columns]
@@ -319,7 +324,7 @@ class TransformerEncoderLayer(nn.Module):
 
 
 # %%
-def mask_tensor(tensor, model, probability=0.8):
+def mask_tensor(tensor, dataset, probability=0.8):
     if tensor.dtype == "float32" or tensor.dtype == "float64":
         is_numeric = True
     elif tensor.dtype == "int32" or tensor.dtype == "int64":
@@ -335,7 +340,9 @@ def mask_tensor(tensor, model, probability=0.8):
         tensor = tensor.at[bit_mask].set(float("-Inf"))
         # tensor[bit_mask] = jnp.array(float("-Inf"))
     else:
-        tensor = tensor.at[bit_mask].set(model.cat_mask_token)
+        cat_mask = dataset.cat_mask_token
+        cat_mask_token = dataset.token_dict[cat_mask]
+        tensor = tensor.at[bit_mask].set(cat_mask_token)
         # tensor[bit_mask] = model.cat_mask_token
     return tensor
 
@@ -357,14 +364,15 @@ class TabTransformer(nn.Module):
         self.col_indices = jnp.array(
             [self.tokens.index(col) for col in self.col_tokens]
         )
+        self.numeric_indices = jnp.array(
+            [self.tokens.index(col) for col in self.dataset.numeric_columns]
+        )
         super().__post_init__()
 
     def setup(self):
         dataset = self.dataset
         n_heads = self.n_heads
         d_model = self.d_model
-        n_tokens = len(dataset.tokens)
-        self.model_name = "Kai is Great"
 
         # self.decoder_dict = {v: k for k, v in self.token_dict.items()}
         # Masks
@@ -378,42 +386,13 @@ class TabTransformer(nn.Module):
         self.col_indices = jnp.array(
             [self.tokens.index(col) for col in self.col_tokens]
         )
-        self.numeric_indices = jnp.array(
-            [self.tokens.index(col) for col in dataset.numeric_columns]
-        )
+
         self.transformer_block1 = TransformerBlock(
             d_model=d_model, num_heads=n_heads, d_ff=d_model * 4, dropout_rate=0.1
         )
         self.transformer_block2 = TransformerBlock(
             d_model=d_model, num_heads=n_heads, d_ff=d_model * 4, dropout_rate=0.1
         )
-        # self.transformer_encoder1 = TransformerEncoderLayer(d_model, n_heads=n_heads)
-        # self.transformer_encoder2 = TransformerEncoderLayer(d_model, n_heads=n_heads)
-        self.regressor = nn.Sequential(
-            [
-                nn.Dense(d_model * 2),
-                nn.relu,  # Error here
-                nn.Dense(1),
-            ]
-            # nn.relu,
-        )
-
-        self.mlm_decoder = nn.Sequential(
-            [nn.Dense(n_tokens)]
-        )  # TODO try making more complex
-
-        self.mnm_decoder = nn.Sequential(
-            [
-                nn.Dense(
-                    self.n_columns * self.d_model, self.d_model * 4
-                ),  # Try making more complex
-                nn.relu,
-                nn.Dense(self.n_numeric_cols),
-            ]
-        )
-
-        self.flatten_layer = nn.Dense(len(self.col_tokens), 1)
-        # self.apply(initialize_parameters)
 
     def __call__(
         self,
@@ -437,28 +416,37 @@ class TabTransformer(nn.Module):
         # )
         numeric_col_embeddings = self.embedding(repeated_numeric_indices)
         inf_mask = numeric_inputs == float("-inf")
-
+        assert inf_mask.shape == numeric_col_embeddings.shape[:2]
         base_numeric = jnp.zeros_like(numeric_col_embeddings)
 
-        num_embeddings = (
-            numeric_col_embeddings[~inf_mask] * numeric_inputs[~inf_mask][:, None]
+        # print(
+        #     f"inf_mask: {inf_mask[:, :, None].shape}",
+        #     f"numeric_col_embeddings: {numeric_col_embeddings.shape}",
+        #     f"numeric_inputs: {numeric_inputs.shape}",
+        #     f"base_numeric: {base_numeric.shape}",
+        # )
+        base_numeric = jnp.where(
+            inf_mask[:, :, None],
+            numeric_col_embeddings * numeric_inputs[:, :, None],
+            base_numeric,
         )
-        # Instead of ``x[idx] = y``, use ``x = x.at[idx].set(y)``
-        base_numeric = base_numeric.at[~inf_mask].set(num_embeddings)
-        base_numeric = base_numeric.at[inf_mask].set(
-            self.embedding(self.numeric_mask_token)
+        # print(f"num_embeddings: {num_embeddings.shape}")
+        # num_embeddings = (
+        #     numeric_col_embeddings[~inf_mask] * numeric_inputs[~inf_mask][:, None]
+        # )
+
+        # base_numeric = base_numeric.at[~inf_mask].set(num_embeddings)
+        base_numeric = jnp.where(
+            inf_mask[:, :, None], self.numeric_mask_token, base_numeric
         )
+        # base_numeric = base_numeric.at[inf_mask].set(
+        #     self.embedding(self.numeric_mask_token)
+        # )
 
         query_embeddings = jnp.concatenate([cat_embeddings, base_numeric], axis=1)
         out = self.transformer_block1(q=col_embeddings, kv=query_embeddings)
         out = self.transformer_block2(X=out)
-        # out = self.transformer_encoder1(
-        #     q=col_embeddings,
-        #     k=query_embeddings,
-        #     v=query_embeddings
-        #     # col_embeddings, query_embeddings, query_embeddings
-        # )
-        # out = self.transformer_encoder2(out, out, out)
+
         return out
 
 
@@ -467,239 +455,88 @@ class MTM(nn.Module):
     d_model: int = 64
     n_heads: int = 4
 
+    def __post_init__(self):
+        self.n_tokens = len(self.dataset.tokens)
+        self.tokens = self.dataset.tokens
+        self.token_dict = self.dataset.token_dict
+        self.cat_mask_token = jnp.array(self.token_dict["[MASK]"])
+        self.numeric_mask_token = jnp.array(self.token_dict["[NUMERIC_MASK]"])
+        self.numeric_columns = self.dataset.numeric_columns
+        self.col_tokens = self.dataset.category_columns + self.dataset.numeric_columns
+        self.col_indices = jnp.array(
+            [self.tokens.index(col) for col in self.col_tokens]
+        )
+        self.numeric_indices = jnp.array(
+            [self.tokens.index(col) for col in self.dataset.numeric_columns]
+        )
+        super().__post_init__()
+
     @nn.compact
-    def __call__(self, numeric_inputs: jnp.array, categorical_inputs: jnp.array):
+    def __call__(
+        self,
+        numeric_inputs: jnp.array,
+        categorical_inputs: jnp.array,
+    ):
         out = TabTransformer(self.dataset, self.d_model, self.n_heads)(
-            numeric_inputs, categorical_inputs
+            numeric_inputs=numeric_inputs, categorical_inputs=categorical_inputs
         )
-        cat_out = self.mlm_decoder(out)
-
+        categorical_out = nn.Dense(
+            name="categorical_out", features=self.dataset.n_tokens
+        )(out)
         numeric_out = jnp.reshape(out, (out.shape[0], -1))
-        print(f"numeric_out shape: {numeric_out.shape}")
-        numeric_out = self.mnm_decoder(numeric_out)
-        print(f"numeric_out shape decoded: {numeric_out.shape}")
-        return cat_out, numeric_out
-
-
-# %%
-def show_mask_pred(i, model, dataset, probability):
-    numeric_values = dataset.X_train_numeric[i : i + 1, :]
-    categorical_values = dataset.X_train_categorical[i : i + 1, :]
-    numeric_masked = mask_tensor(numeric_values, model, probability=probability)
-    categorical_masked = mask_tensor(categorical_values, model, probability=probability)
-    # Predictions
-
-    cat_preds, numeric_preds = model(numeric_masked, categorical_masked, task="mlm")
-    # Get the predicted tokens from cat_preds
-    cat_preds = cat_preds.argmax(dim=2)
-    # Get the words from the tokens
-    decoder_dict = dataset.token_decoder_dict
-    cat_preds = [decoder_dict[i.item()] for i in cat_preds[0]]
-
-    results_dict = {k: cat_preds[i] for i, k in enumerate(model.col_tokens)}
-    for i, k in enumerate(model.col_tokens[model.n_cat_cols :]):
-        results_dict[k] = numeric_preds[0][i].item()
-    # Get the masked values
-    categorical_masked = [decoder_dict[i.item()] for i in categorical_masked[0]]
-    numeric_masked = numeric_masked[0].tolist()
-    masked_values = categorical_masked + numeric_masked
-    # zip the masked values with the column names
-    masked_values = dict(zip(model.col_tokens, masked_values))
-    # Get the original values
-    categorical_values = [decoder_dict[i.item()] for i in categorical_values[0]]
-    numeric_values = numeric_values[0].tolist()
-    original_values = categorical_values + numeric_values
-    # zip the original values with the column names
-    original_values = dict(zip(model.col_tokens, original_values))
-    # print(numeric_masked)
-    # print(categorical_masked)
-    result_dict = {
-        "actual": original_values,
-        "masked": masked_values,
-        "pred": results_dict,
-    }
-
-    return result_dict
-
-
-# %%
-def mtm(model, dataset, model_name, epochs=100, batch_size=1000, lr=0.001):
-    # Masked Tabular Modeling
-    mse_loss = nn.MSELoss()
-    ce_loss = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    numeric_loss_scaler = 15
-    summary_writer = SummaryWriter("runs/" + model_name)
-    early_stopping = EarlyStopping(patience=20, min_delta=0.0001)
-    batch_count = 0
-    model.train()
-    # Tqdm for progress bar with loss and epochs displayed
-    pbar = trange(epochs, desc="Epochs", leave=True)
-    for epoch in pbar:  # trange(epochs, desc="Epochs", leave=True):
-        for i in range(0, dataset.X_train_numeric.size(0), batch_size):
-            numeric_values = dataset.X_train_numeric[i : i + batch_size, :]
-            categorical_values = dataset.X_train_categorical[i : i + batch_size, :]
-            numeric_masked = mask_tensor(numeric_values, model, probability=0.8)
-            categorical_masked = mask_tensor(categorical_values, model, probability=0.8)
-            optimizer.zero_grad()
-            cat_preds, numeric_preds = model(
-                numeric_masked, categorical_masked, task="mlm"
-            )
-            cat_targets = torch.cat(
-                (
-                    categorical_values,
-                    model.numeric_indices.expand(categorical_values.size(0), -1),
+        numeric_out = nn.Sequential(
+            [
+                nn.Dense(name="numeric_dense1", features=self.d_model * 4),
+                nn.relu,
+                nn.Dense(
+                    name="numeric_dense2", features=len(self.dataset.numeric_columns)
                 ),
-                dim=1,
-            )
+            ]
+        )(numeric_out)
 
-            cat_preds = cat_preds.permute(0, 2, 1)  # TODO investigate as possible bug
+        return categorical_out, numeric_out
 
-            cat_loss = ce_loss(cat_preds, cat_targets)
-            numeric_loss = (
-                mse_loss(numeric_preds, numeric_values) * numeric_loss_scaler
-            )  # Hyper param
-            loss = cat_loss + numeric_loss  # TODO Look at scaling
-            loss.backward()
-            optimizer.step()
-            batch_count += 1
-            learning_rate = optimizer.param_groups[0]["lr"]
-            summary_writer.add_scalar("LossTrain/agg_mask", loss.item(), batch_count)
-            summary_writer.add_scalar(
-                "LossTrain/mlm_loss", cat_loss.item(), batch_count
-            )
-            summary_writer.add_scalar(
-                "LossTrain/mnm_loss", numeric_loss.item(), batch_count
-            )
-            summary_writer.add_scalar("Metrics/mtm_lr", learning_rate, batch_count)
 
-        with torch.no_grad():
-            numeric_values = dataset.X_test_numeric
-            categorical_values = dataset.X_test_categorical
-            numeric_masked = mask_tensor(numeric_values, model, probability=0.8)
-            categorical_masked = mask_tensor(categorical_values, model, probability=0.8)
-            optimizer.zero_grad()
-            cat_preds, numeric_preds = model(
-                numeric_masked, categorical_masked, task="mlm"
-            )
-            cat_targets = torch.cat(
-                (
-                    categorical_values,
-                    model.numeric_indices.expand(categorical_values.size(0), -1),
-                ),
-                dim=1,
-            )
+class TRM(nn.Module):
+    dataset: TabularDS
+    d_model: int = 64
+    n_heads: int = 4
 
-            cat_preds = cat_preds.permute(0, 2, 1)
-
-            test_cat_loss = ce_loss(cat_preds, cat_targets)
-            test_numeric_loss = (
-                mse_loss(numeric_preds, numeric_values) * numeric_loss_scaler
-            )  # Hyper param
-            test_loss = test_cat_loss + test_numeric_loss
-
-        summary_writer.add_scalar("LossTest/agg_loss", test_loss.item(), batch_count)
-
-        summary_writer.add_scalar(
-            "LossTest/mlm_loss", test_cat_loss.item(), batch_count
+    def __post_init__(self):
+        self.n_tokens = len(self.dataset.tokens)
+        self.tokens = self.dataset.tokens
+        self.token_dict = self.dataset.token_dict
+        self.cat_mask_token = jnp.array(self.token_dict["[MASK]"])
+        self.numeric_mask_token = jnp.array(self.token_dict["[NUMERIC_MASK]"])
+        self.numeric_columns = self.dataset.numeric_columns
+        self.col_tokens = self.dataset.category_columns + self.dataset.numeric_columns
+        self.col_indices = jnp.array(
+            [self.tokens.index(col) for col in self.col_tokens]
         )
-        summary_writer.add_scalar(
-            "LossTest/mnm_loss", test_numeric_loss.item(), batch_count
+        self.numeric_indices = jnp.array(
+            [self.tokens.index(col) for col in self.dataset.numeric_columns]
         )
-        early_stopping_status = early_stopping(model, test_loss)
-        pbar.set_description(
-            f"Epoch {epoch+1}/{epochs} Loss: {loss.item():,.4f} "
-            + f"Test Loss: {test_loss.item():,.4f}"
-            + f" Early Stopping: {early_stopping.status}"
+        super().__post_init__()
+
+    @nn.compact
+    def __call__(
+        self,
+        numeric_inputs: jnp.array,
+        categorical_inputs: jnp.array,
+    ):
+        out = TabTransformer(self.dataset, self.d_model, self.n_heads)(
+            numeric_inputs=numeric_inputs, categorical_inputs=categorical_inputs
         )
-        if early_stopping_status:
-            break
+
+        out = nn.Sequential(
+            [
+                nn.Dense(name="dense1", features=self.d_model * 2),
+                nn.relu,
+                nn.Dense(name="dense2", features=1),
+            ]
+        )(out)
+        out = jnp.reshape(out, (out.shape[0], -1))
+        return out
 
 
 # %%
-def fine_tune_model(
-    model, dataset, n_rows, model_name, epochs=100, lr=0.01, early_stop=True
-):
-    # Regression Model
-
-    batch_size = 1000
-
-    loss_fn = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-    model_time = dt.now().strftime("%Y-%m-%dT%H:%M:%S")
-    model_name = f"{model_name}_{n_rows}_{model_time}"
-    if early_stop:
-        early_stopping = EarlyStopping(patience=50, min_delta=0.0001)
-    else:
-        early_stopping = None
-    summary_writer = SummaryWriter("runs/" + model_name)
-    if n_rows is None:
-        n_rows = dataset.X_train_numeric.size(0)
-    if n_rows > dataset.X_train_numeric.size(0):
-        raise ValueError(
-            f"n_rows ({n_rows}) must be less than or equal to "
-            + f"{dataset.X_train_numeric.size(0)}"
-        )
-
-    train_set_size = n_rows  # dataset.X_train_numeric.size(0)
-    batch_count = 0
-    model.train()
-    pbar = trange(epochs, desc=f"Epochs, Model: {model_name}")
-    if batch_size > train_set_size:
-        batch_size = train_set_size
-    for epoch in pbar:
-        for i in range(0, train_set_size, batch_size):
-            num_inputs = dataset.X_train_numeric[i : i + batch_size, :]
-            cat_inputs = dataset.X_train_categorical[i : i + batch_size, :]
-            optimizer.zero_grad()
-            y_pred = model(num_inputs, cat_inputs)
-            loss = loss_fn(y_pred, dataset.y_train[i : i + batch_size, :])
-            loss.backward()
-            optimizer.step()
-            batch_count += 1
-            learning_rate = optimizer.param_groups[0]["lr"]
-            summary_writer.add_scalar(
-                "LossTrain/regression_loss", loss.item(), batch_count
-            )
-            summary_writer.add_scalar(
-                "Metrics/regression_lr", learning_rate, batch_count
-            )
-
-        # Test set
-        with torch.no_grad():
-            y_pred = model(dataset.X_test_numeric, dataset.X_test_categorical)
-            loss_test = loss_fn(y_pred, dataset.y_test)
-            summary_writer.add_scalar(
-                "LossTest/regression_loss", loss_test.item(), batch_count
-            )
-        if early_stopping is not None:
-            early_stopping_status = early_stopping(model, loss_test)
-        else:
-            early_stopping_status = False
-        pbar.set_description(
-            f"Epoch {epoch+1}/{epochs} "
-            + f"n_rows: {n_rows:,} "
-            + f"Loss: {loss.item():,.2f} "
-            + f"Test Loss: {loss_test.item():,.2f} "
-            + f"Early Stopping: {early_stopping_status}"
-        )
-        if early_stopping_status:
-            break
-    best_loss = early_stopping.best_loss if early_stopping is not None else loss_test
-    return {"n_rows": n_rows, "test_loss": best_loss.item()}
-
-
-# %%
-
-
-def regression_actuals_preds(model, dataset):
-    with torch.no_grad():
-        y_pred = model(
-            dataset.X_test_numeric, dataset.X_test_categorical, task="regression"
-        )
-        y = dataset.y_test
-    df = pd.DataFrame({"actual": y.flatten(), "pred": y_pred.flatten()})
-    df["pred"] = df["pred"].round()
-    df["error"] = df["actual"] - df["pred"]
-    return df
