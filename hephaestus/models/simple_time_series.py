c625f7861f69e9d8e15d418174bdd8c816533a65
Kai Lukowiak <kailukowiak@gmail.com>
2024-05-26 21:28:52 -0600

    not workign

--- hephaestus/models/simple_time_series.py
+++ hephaestus/models/simple_time_series.py
@@ -1,17 +1,57 @@
 # %%
 # import jax
+
+
 import jax.numpy as jnp
+import numpy as np
 from flax import linen as nn
 from icecream import ic
 from jax.lax import stop_gradient
-
-from ..utils.data_utils import TabularDS
-
-ic.configureOutput(includeContext=True, contextAbsPath=True)
+from torch.utils.data import Dataset
 
 
-# ic.disable()
-# %%
+class SimpleDS(Dataset):
+    def __init__(self, df, custom_attention: bool = True):
+        # Add nan padding to make sure all sequences are the same length
+        # use the idx column to group by
+        self.max_seq_len = df.groupby("idx").count().time_step.max()
+        self.custom_attention = custom_attention
+        self.df = df
+        self.batch_size = self.max_seq_len
+
+        self.special_tokens = ["[PAD]", "[NUMERIC_MASK]", "[MASK]"]
+        self.cat_mask = "[MASK]"
+        self.numeric_mask = "[NUMERIC_MASK]"
+
+        self.col_tokens = [col_name for col_name in df.columns if col_name != "idx"]
+
+        self.tokens = self.special_tokens + self.col_tokens
+
+        self.token_dict = {token: i for i, token in enumerate(self.tokens)}
+        self.token_decoder_dict = {i: token for i, token in enumerate(self.tokens)}
+        self.n_tokens = len(self.tokens)
+        self.numeric_indices = jnp.array(
+            [self.tokens.index(i) for i in self.col_tokens]
+        )
+
+        self.numeric_mask_token = self.tokens.index(self.numeric_mask)
+
+    def __len__(self):
+        return self.df.idx.max() + 1  # probably should be max idx + 1 thanks
+
+    def __getitem__(self, set_idx):
+        batch = self.df.loc[
+            self.df.idx == set_idx, [col for col in self.df.columns if col != "idx"]
+        ]
+        batch = np.array(batch.values)
+        # Add padding
+        batch_len, n_cols = batch.shape
+        pad_len = self.max_seq_len - batch_len
+        padding = np.full((pad_len, n_cols), jnp.nan)
+        batch = np.concatenate([batch, padding], axis=0)
+        return batch
+
+
 class FeedForwardNetwork(nn.Module):
     d_model: int
     d_ff: int
@@ -35,16 +75,23 @@ class TransformerBlock(nn.Module):
     dropout_rate: float
 
     @nn.compact
-    def __call__(self, q: jnp.array, k: jnp.array, v: jnp.array, deterministic: bool):
+    def __call__(
+        self,
+        q: jnp.array,
+        k: jnp.array,
+        v: jnp.array,
+        deterministic: bool,
+        mask: jnp.array = None,
+    ):
         # Multi-head self-attention
-        causal_mask = causal_mask = nn.make_causal_mask(q[:, :, :, 0])
+        # causal_mask = causal_mask = nn.make_causal_mask(q[:, :, :, 0])
 
         # Write out the jax array to a file for more debugging
         attention = nn.MultiHeadDotProductAttention(
             num_heads=self.num_heads,
             qkv_features=self.d_model,
             dropout_rate=self.dropout_rate,
-        )(q, k, v, deterministic=deterministic, mask=causal_mask)
+        )(q, k, v, deterministic=deterministic, mask=mask)
         out = q + attention
         out = nn.LayerNorm()(out)
         # Feed Forward Network
@@ -54,6 +101,67 @@ class TransformerBlock(nn.Module):
         out = out + ffn
         out = nn.LayerNorm()(out)
         return out
+
+
+# def create_masks(batch_input):
+#     # Input shape: (batch_size, n_rows, n_columns, embedding_dim)
+#     batch_size, n_rows, n_columns = batch_input.shape
+
+#     # Create padding mask for jnp.nan values
+#     padding_mask = ~jnp.isnan(batch_input).any(
+#         axis=-1
+#     )  # shape: (batch_size, n_rows, n_columns)
+
+#     # Create a causal mask
+#     causal_mask = jnp.tril(jnp.ones((n_rows, n_rows)), k=0)  # shape: (n_rows, n_rows)
+
+#     # Combine the masks
+#     combined_mask = jnp.logical_and(
+#         padding_mask[..., jnp.newaxis], causal_mask[jnp.newaxis, :, :]
+#     )
+
+
+#     # The combined_mask will be of shape: (batch_size, n_rows, n_columns, n_rows)
+#     return combined_mask
+def create_padding_mask(batch_input):
+    # Input shape: (batch_size, n_rows, n_columns, embedding_dim)
+    padding_mask = ~jnp.isnan(batch_input).any(
+        axis=-1
+    )  # shape: (batch_size, n_rows, n_columns)
+    padding_mask = jnp.all(padding_mask, axis=-1)  # shape: (batch_size, n_rows)
+    return padding_mask[
+        :, jnp.newaxis, jnp.newaxis, :
+    ]  # shape: (batch_size, 1, 1, n_rows)
+
+
+def create_causal_mask(n_rows):
+    return jnp.tril(
+        jnp.ones((n_rows, n_rows), dtype=bool), k=0
+    )  # shape: (n_rows, n_rows)
+
+
+def combine_masks(padding_mask, causal_mask):
+    # Ensure both masks are boolean
+    padding_mask = padding_mask.astype(bool)
+    causal_mask = causal_mask.astype(bool)
+
+    # Expand causal mask to match the batch size and heads dimension
+    batch_size = padding_mask.shape[0]
+    causal_mask = causal_mask[
+        jnp.newaxis, jnp.newaxis, :, :
+    ]  # shape: (1, 1, n_rows, n_rows)
+
+    # Combine the masks
+    combined_mask = padding_mask & causal_mask  # shape: (batch_size, 1, n_rows, n_rows)
+    return combined_mask
+
+
+def create_masks(batch_input):
+    n_rows = batch_input.shape[1]
+    padding_mask = create_padding_mask(batch_input)
+    causal_mask = create_causal_mask(n_rows)
+    combined_mask = combine_masks(padding_mask, causal_mask)
+    return combined_mask
 
 
 class TimeSeriesTransformer(nn.Module):
@@ -86,7 +194,7 @@ class TimeSeriesTransformer(nn.Module):
         >>> output = transformer(numeric_inputs)
     """
 
-    dataset: TabularDS
+    dataset: SimpleDS
     d_model: int = 64
     n_heads: int = 4
     time_window: int = 10_000
@@ -100,7 +208,12 @@ class TimeSeriesTransformer(nn.Module):
         )
         # numeric_indices = jnp.array([i for i in range(len(self.dataset.df.columns))])
         # Embed column indices
-
+        # causal_mask = create_causal_mask(numeric_inputs)
+        # attention_mask = create_padding_mask(numeric_inputs)
+        # mask = combine_masks(attention_mask, causal_mask)
+        mask = create_masks(numeric_inputs)
+        ic(mask.shape)
+        # causal_mask = nn.make_causal_mask(numeric_inputs[:, :, :, 0])
         col_embeddings = embedding(self.dataset.numeric_indices)
         ic(col_embeddings.shape, numeric_inputs.shape)
         repeated_numeric_indices = jnp.tile(
@@ -124,13 +237,13 @@ class TimeSeriesTransformer(nn.Module):
         # numeric_inputs = stop_gradient(jnp.where(nan_mask, 0.0, numeric_inputs))
         numeric_broadcast = numeric_inputs[:, :, :, None] * numeric_col_embeddings
         # numeric_inputs = numeric_inputs[:, :, :, None]
-        ic(
-            "here!!!!!!",
-            numeric_inputs.shape,
-            numeric_col_embeddings.shape,
-            nan_mask.shape,
-            numeric_broadcast.shape,
-        )
+        # ic(
+        #     "here!!!!!!",
+        #     numeric_inputs.shape,
+        #     numeric_col_embeddings.shape,
+        #     nan_mask.shape,
+        #     numeric_broadcast.shape,
+        # )
         numeric_broadcast = jnp.where(
             # nan_mask,
             jnp.expand_dims(nan_mask, axis=-1),
@@ -159,6 +272,7 @@ class TimeSeriesTransformer(nn.Module):
             k=numeric_col_embeddings,
             v=numeric_broadcast,  # TODO differentiate this
             deterministic=deterministic,
+            mask=mask,
         )
 
         # ic(f"Nan values in out 1st mha: {jnp.isnan(out).any()}")
@@ -167,7 +281,7 @@ class TimeSeriesTransformer(nn.Module):
             num_heads=self.n_heads,
             d_ff=64,
             dropout_rate=0.1,
-        )(q=out, k=out, v=out, deterministic=deterministic)
+        )(q=out, k=out, v=out, deterministic=deterministic, mask=mask)
 
         # ic(f"Nan values in in out 2nd mha: {jnp.isnan(out).any()}")
 
@@ -175,7 +289,7 @@ class TimeSeriesTransformer(nn.Module):
 
 
 class SimplePred(nn.Module):
-    dataset: TabularDS
+    dataset: SimpleDS
     d_model: int = 64 * 10
     n_heads: int = 4
 
