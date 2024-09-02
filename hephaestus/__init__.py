e7b1e9aaed5a78d5983616d4215273332b5d7375
Kai Lukowiak <kailukowiak@gmail.com>
2024-09-01 18:35:53 -0600

    moving towards cross entropy

--- hephaestus/__init__.py
+++ hephaestus/__init__.py
@@ -1,8 +1,8 @@
-from .models import simple_time_series  # noqa: F401, F403
-from .models import time_series as time_series  # noqa: F401, F403
+from .models import time_series_decoder  # noqa: F401, F403
 from .models.models import *  # noqa: F401, F403
 from .utils.data_utils import *  # noqa: F401, F403
 from .utils.mtm_training import *  # noqa: F401, F403
-from .utils.timeseries_training import *  # noqa: F401, F403
+
+# from .utils.timeseries_training import *  # noqa: F401, F403
 from .utils.trm_training import *  # noqa: F401, F403
 from .utils.tts_training import *  # noqa: F401, F403  # noqa: F401, F403
