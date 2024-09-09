e9a2d8c4af57a429b6830fe9f116d7deedf3762a
Kai Lukowiak <kailukowiak@gmail.com>
2024-09-08 20:04:53 -0500

    removed superfluous todos

--- hephaestus/models/models.py
+++ hephaestus/models/models.py
@@ -94,7 +94,6 @@ class TimeSeriesTransformerModel(nn.Module):
         # Embed column indices
         col_embeddings = embedding(jnp.array(self.dataset.col_indices))
         cat_embeddings = embedding(categorical_inputs)
-        # TODO implement no grad here
         repeated_numeric_indices = jnp.tile(
             self.dataset.numeric_indices, (numeric_inputs.shape[0], 1)
         )
@@ -164,7 +163,6 @@ class TabTransformer(nn.Module):
         )
         col_embeddings = embedding(repeated_col_indices)
         cat_embeddings = embedding(categorical_inputs)
-        # TODO implement no grad here
         repeated_numeric_indices = jnp.tile(
             self.dataset.numeric_indices, (numeric_inputs.shape[0], 1)
         )
