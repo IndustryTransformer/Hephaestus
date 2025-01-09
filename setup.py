29e896928ca43b5055ce844c106ae35ff7da4c05
Kai Lukowiak <kailukowiak@gmail.com>
2025-01-08 21:58:55 -0700

    cleaning more

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="hephaestus",
     packages=find_packages(),
+    version="0.1.0",
+    description="Tabular Transformers at your fingertips",
+    author="Kai Lukowiak",
+    author_email="kai.lukowiak@industrytransformer.io",
     # other setup configurations...
 )
