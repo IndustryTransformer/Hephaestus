238321def1ca60c4733853891fd4b542587c3a50
Kai Lukowiak <kailukowiak@gmail.com>
2025-02-22 18:16:28 +0000

    Refactor Plannets module: streamline imports, disable icecream logging, and replace subprocess calls with hephaestus functions

--- plannets.py
+++ plannets.py
@@ -2,17 +2,13 @@
 import ast
 import os
 import re
-import subprocess
 from datetime import datetime as dt
 
 import icecream
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from flax import nnx
-from flax.struct import dataclass
 from icecream import ic
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
@@ -20,10 +16,10 @@ from tqdm.notebook import tqdm, trange
 from transformers import BertTokenizerFast, FlaxBertModel
 
 import hephaestus as hp
-import hephaestus.training.training as ht
+import hephaestus.training as ht
 
 icecream.install()
-ic_disable = False  # Global variable to disable ic
+ic_disable = True  # Global variable to disable ic
 if ic_disable:
     ic.disable()
 ic.configureOutput(includeContext=True, contextAbsPath=True)
@@ -196,18 +192,7 @@ ic.disable()
 causal_mask = True
 # time_series_regressor.train()
 
-
 # %%
-def get_git_commit_hash():
-    try:
-        commit_hash = (
-            subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
-            .strip()
-            .decode()
-        )
-        return commit_hash
-    except Exception:
-        return "unknown"
 
 
 # %%
@@ -221,7 +206,7 @@ metrics = ht.create_metrics()
 writer_name = "LowerRate"
 # Get git commit hash for model name?
 writer_time = dt.now().strftime("%Y-%m-%dT%H:%M:%S")
-commit_hash = get_git_commit_hash()
+commit_hash = hp.get_git_commit_hash()
 model_name = f"{writer_time}_{writer_name}_{commit_hash}"
 summary_writer = SummaryWriter("runs/" + model_name)
 
@@ -254,90 +239,16 @@ for step, batch in enumerate(tqdm(train_data_loader)):
 # checkpointer = ocp.StandardCheckpointer()
 # checkpointer.save(ckpt_dir / "state", state)
 
-
 # %%
-@dataclass
-class Results:
-    numeric_out: jnp.array
-    categorical_out: jnp.array
-    numeric_inputs: jnp.array
-    categorical_inputs: jnp.array
-
-
-def return_results(model, dataset, idx=0, mask_start: int = None):
-    numeric_inputs, categorical_inputs = dataset[idx]
-    if mask_start:
-        numeric_inputs = numeric_inputs[:, :mask_start]
-        categorical_inputs = categorical_inputs[:, :mask_start]
-    numeric_inputs = jnp.array([numeric_inputs])
-    categorical_inputs = jnp.array([categorical_inputs])
-    out = time_series_regressor(
-        numeric_inputs=numeric_inputs, categorical_inputs=categorical_inputs
-    )
-    numeric_out, categorical_out = out["numeric_out"], out["categorical_out"]
-    return Results(numeric_out, categorical_out, numeric_inputs, categorical_inputs)
-
-
-x = return_results(time_series_regressor, train_ds, 0)
+x = hp.return_results(time_series_regressor, train_ds, 0)
 x.categorical_out.shape
-
-# %%
-
 
 # %%
 causal_mask = False
 causal_mask = True
 
 
-def process_results(arr: jnp.array, col_names: list, config: hp.TimeSeriesConfig):
-    arr = jnp.squeeze(arr)
-    if arr.ndim == 3:
-        # Check if there is a logit array for example if there are 3 dims then the
-        # last dim is the logit array. We need to get the argmax of the last dim
-        # to get the actual values of the array and replace the logit array with the
-        # actual values
-        arr = jnp.argmax(arr, axis=-1)
-    df = pd.DataFrame(arr.T)
-    df.columns = col_names
-    return df
-
-
-@dataclass
-class DFComparison:
-    input_df: pd.DataFrame
-    output_df: pd.DataFrame
-
-
-def show_results_df(
-    model, time_series_config, dataset, idx: int = 0, mask_start: int = None
-):
-    results = return_results(model, dataset, idx=idx, mask_start=mask_start)
-
-    input_categorical = process_results(
-        results.categorical_inputs,
-        time_series_config.categorical_col_tokens,
-        time_series_config,
-    )
-    input_numeric = process_results(
-        results.numeric_inputs,
-        time_series_config.numeric_col_tokens,
-        time_series_config,
-    )
-    output_categorical = process_results(
-        results.categorical_out,
-        time_series_config.categorical_col_tokens,
-        time_series_config,
-    )
-    output_numeric = process_results(
-        results.numeric_out, time_series_config.numeric_col_tokens, time_series_config
-    )
-    input_df = pd.concat([input_categorical, input_numeric], axis=1)
-    output_df = pd.concat([output_categorical, output_numeric], axis=1)
-
-    return DFComparison(input_df, output_df)
-
-
-df_comp = show_results_df(
+df_comp = hp.show_results_df(
     model=time_series_regressor,
     time_series_config=time_series_config,
     dataset=train_ds,
@@ -350,162 +261,23 @@ df_comp.output_df.loc[:, time_series_config.categorical_col_tokens].tail()
 # %%
 df_comp.output_df.loc[:, time_series_config.numeric_col_tokens].tail()
 
-
 # %%
-def plot_planets(df_pred: pd.DataFrame, df_actual: pd.DataFrame, column: str, offset=0):
-    plt.figure(figsize=(15, 10))
-    plt.plot(df_pred[column], label="Autogregressive")
-    plt.plot(df_actual[column], label="Actual")
-    plt.title(f"{column} Predictions")
-    plt.legend()
-    # Show ticks and grid lines every 1 step
-    plt.xticks(np.arange(0, len(df_pred), 1))
-    plt.grid()
-    # add black line at 0 on the y axis to show the difference
-    plt.axhline(0, color="black")
-    plt.show()
-
-
-# %%
-@dataclass
-class AutoRegressiveResults:
-    numeric_inputs: jnp.array
-    categorical_inputs: jnp.array
-
-    @classmethod
-    def from_ds(cls, ds: hp.TimeSeriesDS, idx: int, stop_idx: int = 10):
-        inputs = ds[idx]
-        numeric_inputs = inputs[0][:, :stop_idx]
-        categorical_inputs = inputs[1][:, :stop_idx]
-        return cls(numeric_inputs, categorical_inputs)
-
-
-def auto_regressive_predictions(
-    model: hp.TimeSeriesDecoder,
-    inputs: AutoRegressiveResults,
-) -> jnp.array:
-    numeric_inputs = inputs.numeric_inputs
-    categorical_inputs = inputs.categorical_inputs
-
-    # get the first row that contains all nan vales
-    # if nan_rows_start >= stop_idx:
-    #     return inputs
-    # numeric_inputs = inputs.numeric_inputs
-    # categorical_inputs = inputs.categorical_inputs
-
-    # Expand first dim to make it a batch
-    if numeric_inputs.ndim == 2:
-        print("Expanding dims")
-        numeric_inputs = jnp.array(jnp.array([numeric_inputs]))
-        categorical_inputs = jnp.array([categorical_inputs])
-
-    numeric_nan_columns = jnp.isnan(numeric_inputs).all(axis=2)
-    categorical_nan_columns = jnp.isnan(categorical_inputs).all(axis=2)
-
-    outputs = time_series_regressor(
-        numeric_inputs=numeric_inputs, categorical_inputs=categorical_inputs
-    )
-    numeric_out = outputs["numeric_out"]  #  jnp.squeeze(outputs["numeric_out"])
-    # print(f"{numeric_out.shape=}")
-    categorical_out = outputs[
-        "categorical_out"
-    ]  # jnp.squeeze(outputs["categorical_out"])
-
-    categorical_out = jnp.argmax(categorical_out, axis=-1)
-
-    final_numeric_row = np.array(numeric_out[:, :, -1])
-
-    final_numeric_row = final_numeric_row[None, :]  # New axis
-    final_numeric_row = jnp.transpose(final_numeric_row, (0, 2, 1))
-    # print(f"{final_numeric_row.shape=}")
-    final_categorical_row = np.array(categorical_out[:, :, -1])
-    final_categorical_row = final_categorical_row[:, None]  # New axis
-    final_categorical_row = jnp.transpose(final_categorical_row, (0, 2, 1))
-    # print(f"{numeric_inputs.shape=}, {final_numeric_row.shape=}")
-    numeric_inputs = jnp.concatenate(
-        [numeric_inputs, final_numeric_row], axis=2
-    )  # here
-
-    categorical_inputs = jnp.concatenate(
-        [categorical_inputs, final_categorical_row], axis=2
-    )
-    numeric_inputs = numeric_inputs.at[jnp.array(numeric_nan_columns)].set(jnp.nan)
-    categorical_inputs = categorical_inputs.at[jnp.array(categorical_nan_columns)].set(
-        jnp.nan
-    )
-    inputs = (numeric_inputs, categorical_inputs)
-
-    return AutoRegressiveResults(numeric_inputs, categorical_inputs)
-    # return auto_regressive_predictions(state, inputs, stop_idx)
-
-
-test_inputs = AutoRegressiveResults.from_ds(train_ds, 0, 13)
+test_inputs = hp.AutoRegressiveResults.from_ds(train_ds, 0, 13)
 
 # inputs_test = train_ds[0]
 # test_numeric = inputs_test[0]
 # test_categorical = inputs_test[1]
 # print(inputs_test.shape)
 for i in trange(21):
-    test_inputs = auto_regressive_predictions(time_series_regressor, test_inputs)
+    test_inputs = hp.auto_regressive_predictions(time_series_regressor, test_inputs)
 
 # x = auto_regressive_predictions(state, test_ds[0], 10)
 
-
 # %%
-def create_test_inputs_df(test_inputs, time_series_config):
-    # Extract numeric and categorical inputs from test_inputs
-    numeric_inputs = test_inputs.numeric_inputs
-    categorical_inputs = test_inputs.categorical_inputs
-    numeric_inputs = jnp.squeeze(numeric_inputs)
-    categorical_inputs = jnp.squeeze(categorical_inputs)
-    # Get column names from time_series_config
-    numeric_col_names = time_series_config.numeric_col_tokens
-    categorical_col_names = time_series_config.categorical_col_tokens
-
-    # Create DataFrames for numeric and categorical inputs
-    numeric_df = pd.DataFrame(numeric_inputs.T, columns=numeric_col_names)
-    categorical_df = pd.DataFrame(categorical_inputs.T, columns=categorical_col_names)
-
-    # Combine the DataFrames
-    test_inputs_df = pd.concat([numeric_df, categorical_df], axis=1)
-    return test_inputs_df
-
-
-auto_df = create_test_inputs_df(test_inputs, time_series_config)
+auto_df = hp.create_test_inputs_df(test_inputs, time_series_config)
 
 # %%
 auto_df.tail()
-
-
-# %%
-def plot_comparison(actual_df, one_off_auto_df, auto_regressive_df, column):
-    plt.figure(figsize=(15, 10))
-
-    # Plot actual data
-    sns.lineplot(data=actual_df, x=actual_df.index, y=column, label="Actual")
-
-    # Plot one-off auto data
-    sns.lineplot(
-        data=one_off_auto_df, x=one_off_auto_df.index, y=column, label="One-Off Auto"
-    )
-
-    # Plot auto-regressive data
-    sns.lineplot(
-        data=auto_regressive_df,
-        x=auto_regressive_df.index,
-        y=column,
-        label="Auto-Regressive",
-    )
-
-    plt.title(f"Comparison of {column}")
-    plt.xlabel("Row Index")
-    plt.ylabel(column)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-
-# Example usage:
 
 # %% [markdown]
 #
@@ -517,36 +289,31 @@ res1 = time_series_regressor(
 )
 
 
-def create_non_auto_df(res, time_series_config):
-    numeric_out = res["numeric_out"]
-    categorical_out = res["categorical_out"]
-    numeric_df = process_results(
-        numeric_out, time_series_config.numeric_col_tokens, time_series_config
-    )
-    categorical_df = process_results(
-        categorical_out,
-        time_series_config.categorical_col_tokens,
-        time_series_config,
-    )
-    return pd.concat([categorical_df, numeric_df], axis=1)
-
-
-res_df = create_non_auto_df(res1, time_series_config)
-actual_df = create_non_auto_df(
+res_df = hp.create_non_auto_df(res1, time_series_config)
+actual_df = hp.create_non_auto_df(
     {"numeric_out": train_ds[0][0], "categorical_out": train_ds[0][1]},
     time_series_config,
 )
 
 # %%
-create_non_auto_df(
+hp.create_non_auto_df(
     {"numeric_out": train_ds[0][0], "categorical_out": train_ds[0][1]},
     time_series_config,
 )
 
 # %%
-plot_comparison(actual_df, auto_df, res_df, "planet0_x")
+hp.plot_comparison(actual_df, auto_df, res_df, "planet0_x")
 
 # %%
 test_inputs.numeric_inputs.shape
+
+# %%
+
+
+# %%
+
+
+# %%
+
 
 # %%
