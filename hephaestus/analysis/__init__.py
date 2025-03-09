684baf796a0b024f0c1e3c6bb4d7af87876295d2
Kai Lukowiak <kailukowiak@gmail.com>
2025-03-08 20:54:00 -0700

    Refactor analysis module imports and clean up Plannets notebook

--- hephaestus/analysis/__init__.py
+++ hephaestus/analysis/__init__.py
@@ -6,11 +6,10 @@ from .analysis import (
     auto_regressive_predictions,
     create_non_auto_df,
     create_test_inputs_df,
-    plot_col_error,
-    plot_col_error_comparison,
     plot_column_variants,
     plot_comparison,
     process_results,
     return_results,
     show_results_df,
 )
+from .plots import plot_col_comparison, plot_col_error, plot_training_history
